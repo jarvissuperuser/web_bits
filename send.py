57c9b2b871a7eb06a7a52d4a278771301d89484b
timothy <mugadzatt001@gmail.com>
2018-05-11 15:19:30 +0200

    ubuntu webserver setup

---
+++ send.py
@@ -0,0 +1,29 @@
+#/usr/lib/python2.7
+from os.path import isdir, exists
+from os import makedirs,mkdir, getcwd, system
+def chkdir(dirloc):
+	ex = False
+	if (isdir(dirloc)):
+		ex = True
+	return  ex
+
+def ctd(varn):
+	dl = '/var/www/html/' + varn
+	return chkdir(dl)
+
+def gprj():
+	wd = getcwd()
+	dc = False
+	fn = wd.split('/')
+	fn.reverse()
+	if (ctd(fn[0])):
+		system('cp -a ./* /var/www/html/'+fn[0])
+		system('chown -R www-data:www-data /var/www/html/'+fn[0])
+	else:
+		mkdir('/var/www/html/'+fn[0])
+		system('cp -a ./* /var/www/html/'+fn[0])
+		system('chown -R www-data:www-data /var/www/html/'+fn[0])
+	return 'Copied' 
+
+print str(ctd('dm'))
+print gprj()
